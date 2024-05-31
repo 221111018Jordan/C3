08fdab9a2617f43a38da38c4ccbb4c0b8323efeb
Venderson Egy Agatran <221110627@students.mikroskil.ac.id>
2024-05-31 15:34:27 +0700

    menambahkan banner pada file buttonnavbar

--- lib/screens/buttonNavBar.dart
+++ lib/screens/buttonNavBar.dart
@@ -7,6 +7,8 @@ import 'package:uas/screens/categorypage.dart';
 import 'package:uas/screens/homepage.dart';
 import 'package:provider/provider.dart';
 
+bool lokasi = true;
+
 // ignore: must_be_immutable, camel_case_types
 class bottonnav extends StatefulWidget {
   bottonnav({super.key, required this.currentIndex});
@@ -48,7 +50,8 @@ class _bottonnavState extends State<bottonnav> {
                   });
                 },
                 itemBuilder: (context) => <PopupMenuEntry<String>>[
-                      PopupMenuItem(child:ListTile(title: Text("Dark Mode"),trailing: Switchs())),
+                      PopupMenuItem(child:ListTile(title: Text("Dark Mode"),trailing: Switchs()),padding: EdgeInsets.only(left: 5,right: 5),),
+                      PopupMenuItem(child:Switchslisttile(),padding: EdgeInsets.only(left: 5,right: 5),),
                       PopupMenuDivider(),
                       PopupMenuItem(
                         child: ListTile(leading: Icon(Icons.logout),title: Text("LOG OUT"),),onTap: (){
@@ -87,5 +90,60 @@ class Switchs extends StatelessWidget {
         Provider.of<themeManager>(context, listen: false).changeMode();
       },
     );
+  }
+}
+
+
+class Switchslisttile extends StatefulWidget {
+  Switchslisttile({super.key});
+
+  @override
+  State<Switchslisttile> createState() => _SwitchslisttileState();
+}
+
+class _SwitchslisttileState extends State<Switchslisttile> {
+  @override
+  Widget build(BuildContext context) {
+    return SwitchListTile(
+            value: lokasi,
+            onChanged: (value) {
+              setState(() {
+                lokasi = value;
+              });
+              if (lokasi == false) {
+                ScaffoldMessenger.of(context).showMaterialBanner(MaterialBanner(
+                  content: Text("lokasi kamu terputus,aplikasi harus online"),
+                  actions: [
+                    textbuttonlokasi()
+                  ],
+                  leading: Icon(Icons.location_off),
+                ));
+              } else{
+                ScaffoldMessenger.of(context).hideCurrentMaterialBanner();
+              }
+            },
+            title: Text("lokasi"),
+            secondary: Icon(Icons.location_on),
+          );
+  }
+}
+
+class textbuttonlokasi extends StatefulWidget {
+  const textbuttonlokasi({super.key});
+
+  @override
+  State<textbuttonlokasi> createState() => _textbuttonlokasiState();
+}
+
+class _textbuttonlokasiState extends State<textbuttonlokasi> {
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+                        onPressed: () {
+                          setState(() {
+                            lokasi = true;
+                          });
+                          ScaffoldMessenger.of(context).hideCurrentMaterialBanner();
+                        }, child: Text("nyalakan lokasi"));
   }
 }
