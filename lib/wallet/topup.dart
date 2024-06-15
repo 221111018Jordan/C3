39464e6f648b8fd60134ef2bfd7aed746d7c30aa
Venderson Egy Agatran <221110627@students.mikroskil.ac.id>
2024-06-15 19:07:18 +0700

    menambahkan progress indicator

--- lib/wallet/topup.dart
+++ lib/wallet/topup.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:uas/pages/register.dart';
 import 'package:uas/screens/buttonNavBar.dart';
 import 'package:uas/wallet/wallet.dart';
 import 'package:flutter/services.dart';
@@ -7,6 +8,7 @@ import 'package:intl/intl.dart';
 
 class Walletup extends StatefulWidget {
   Walletup({super.key});
+  bool isLoading = false;
 
   @override
   State<Walletup> createState() => _WalletupState();
@@ -14,6 +16,38 @@ class Walletup extends StatefulWidget {
 
 class _WalletupState extends State<Walletup> {
   TextEditingController jumlah = TextEditingController();
+  bool errorActive = false;
+
+  void generateImage() async {
+    widget.isLoading = true;
+    setState(() {});
+    await Future.delayed(const Duration(seconds: 2), () {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text("berhasil",textAlign: TextAlign.center,),
+              content: SizedBox(
+                  height: 200,
+                  width: double.infinity,
+                  child: Image.network(
+                      "https://st4.depositphotos.com/25868248/29022/v/1600/depositphotos_290222400-stock-illustration-green-check-mark-icon-checkmark.jpg")),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      context.read<Wallet>().topup(_parseCurrency(jumlah.text));
+                      jumlah.text = "";
+                    },
+                    child: Text("close")),
+              ],
+            );
+          });
+
+      widget.isLoading = false;
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,15 +56,12 @@ class _WalletupState extends State<Walletup> {
         title: Text("TOP UP"),
         automaticallyImplyLeading: false,
         actions: [
-          Tooltip(
-            message: 'Kembali ke halaman Home ',
-            child: ElevatedButton(
+          ElevatedButton(
               onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(builder: (context) => bottonnav(currentIndex: 0)));
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => bottonnav(currentIndex: 0)));
               },
-              child: Text("BACK TO HOME"),
-            ),
-          ),
+              child: Text("BACK TO HOME"))
         ],
       ),
       body: Padding(
@@ -45,31 +76,43 @@ class _WalletupState extends State<Walletup> {
             Row(
               children: [
                 Expanded(
-                  child: Tooltip(
-                    message: 'Masukkan Jumlah Uang Yang Tidak melebihi Rp. 100 jt',
-                    child: TextField(
-                      keyboardType: TextInputType.number,
-                      controller: jumlah,
-                      inputFormatters: [CurrencyTextInputFormatter()],
-                      decoration: const InputDecoration(
-                        label: Text("Input Jumlah Topup"),
-                        prefixIcon: Icon(Icons.money),
-                      ),
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    controller: jumlah,
+                    inputFormatters: [CurrencyTextInputFormatter()],
+                    decoration: const InputDecoration(
+                      label: Text("Input Jumlah Topup"),
+                      prefixIcon: Icon(Icons.money),
                     ),
                   ),
                 ),
-                Tooltip(
-                  message: 'Konfirmasi TopUp',
-                  child: ElevatedButton(
+                ElevatedButton(
                     onPressed: () {
-                      context.read<Wallet>().topup(_parseCurrency(jumlah.text));
-                      jumlah.text = "";
+                      setState(() {
+                      if (jumlah.text.isEmpty) {
+                        errorActive = true;
+                      } else {
+                        errorActive = false;
+                        generateImage();
+                      }  
+                      });
                     },
-                    child: Text("Submit"),
-                  ),
-                ),
+                    child: Text("Submit"))
               ],
             ),
+            Center(
+                child: Text(
+              "Harus berisi angka ",
+              style: TextStyle(
+                color: errorActive ? Colors.red : Colors.white,
+              ),
+            )),
+            SizedBox(
+              height: 300,
+              width: 300,
+              child: widget.isLoading ? const Center(
+                child: CircularProgressIndicator(),
+              ):Text("nothing",style: TextStyle(color: Colors.white),)),
           ],
         ),
       ),
@@ -86,7 +129,8 @@ class CurrencyTextInputFormatter extends TextInputFormatter {
   final formatter = NumberFormat.decimalPattern('en_US');
 
   @override
-  TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
     String newText = newValue.text.replaceAll('.', '');
 
     if (newText.isEmpty) {
@@ -98,7 +142,9 @@ class CurrencyTextInputFormatter extends TextInputFormatter {
       return oldValue; // Keep old value if new value exceeds 100,000,000
     }
 
-    final formatted = formatter.format(number).replaceAll(',', '.'); // Replace commas with dots
+    final formatted = formatter
+        .format(number)
+        .replaceAll(',', '.'); // Replace commas with dots
 
     return newValue.copyWith(
       text: formatted,
